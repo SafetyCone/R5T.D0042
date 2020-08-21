449085f73f0feafb38218e246e0c60358f502512
David Coats <david@getsafetycone.com>
2020-08-21 16:02:13 -0700

    Add R5T.D0042.Base library for ITextFileSerializer<T> service definition interface type

---
+++ source/R5T.D0042.Base/Code/Services/Definitions/ITextFileSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace R5T.D0042
+{
+    /// <summary>
+    /// De/serializes an object to/from a text file.
+    /// </summary>
+    /// <remarks>
+    /// * Upon writing, note that the optional overwrite argument default value is true.
+    /// </remarks>
+    public interface ITextFileSerializer<T>
+    {
+        /// <summary>
+        /// Deserializes an object from a text file.
+        /// </summary>
+        /// <param name="filePath">The rooted text file path to use.</param>
+        T Deserialize(string textFilePath);
+
+        /// <summary>
+        /// Serializes an object to a text file.
+        /// </summary>
+        /// <param name="textFilePath">The rooted text file path to use.</param>
+        void Serialize(string textFilePath, T value, bool overwrite = true);
+    }
+}
